a62b83c7f4da014f423afca7c91e5a36364ad19f
shrutikotgire3 <shrutihiramankotgire5@gmail.com>
2026-09-06 17:10:37 +0530

    fix: update Next.js image configuration

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  images:{
-    domains:['places.googleapis.com']
-  }
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "places.googleapis.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
